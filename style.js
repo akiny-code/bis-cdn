680d434024b628c4639b00857876f1feb68c360c
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2024-04-13 20:50:36 +0700

    9

--- style.js
+++ style.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const dir = process.argv[2] ?? 'polaris-icons@9.0.0'
+const dir = process.argv[2] ?? 'polaris-icons-9'
 const pi = process.argv[3] ?? 'pi'
 
 const styles = fs.readdirSync(dir).filter(name => {
@@ -20,6 +20,8 @@ mask-image: url(${name});
 fs.writeFileSync(dir + '/style.css', `.${pi}{
 -webkit-mask-position: center;
 mask-position: center;
+-webkit-mask-repeat: no-repeat;
+mask-repeat: no-repeat;
 }
 .${pi}::before {
 content: "\\25A2";
