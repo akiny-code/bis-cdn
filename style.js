6170ed1bb0cf56c74036d85779308ef52c9da192
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2024-04-13 20:20:32 +0700

    viewbox

--- style.js
+++ style.js
@@ -2,7 +2,17 @@ const fs = require('fs')
 const dir = process.argv[2] ?? 'polaris-icons@9'
 const pi = process.argv[3] ?? 'pi'
 
-const styles = fs.readdirSync(dir).filter(name => name.endsWith('.svg')).map(name =>
+const styles = fs.readdirSync(dir).filter(name => {
+  let result = name.endsWith('.svg')
+  if (result && pi === 'pi') {
+    let path = `${dir}/${name}`
+    fs.writeFileSync(path, fs.readFileSync(path, 'utf-8').replace(
+      'viewBox="0 0 20 20"',
+      'viewBox="2.5 2.5 15 15"'
+    ))
+  }
+  return result
+}).map(name =>
 `.${pi}${name.slice(0, -8).replace(/([A-Z])/g, "-$1").toLowerCase()}{
 -webkit-mask-image: url(${name});
 mask-image: url(${name});
