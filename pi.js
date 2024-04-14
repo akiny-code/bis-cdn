7abae9c518db00470fb48c3a64413cbd1b8b134b
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2024-04-14 07:44:44 +0700

    pi

--- pi.js
+++ pi.js
@@ -5,23 +5,30 @@ const styles = fs.readdirSync(dir).filter(name => {
   let result = name.endsWith('.svg')
   if (result) {
     let path = `${dir}/${name}`
-    fs.writeFileSync(path, fs.readFileSync(path, 'utf-8').replace(
+    let content = fs.readFileSync(path, 'utf-8')
+    if (content.includes('viewBox="0')) fs.writeFileSync(path, content.replace(
       'viewBox="0 0 20 20"',
       'viewBox="2 2 16 16"'
     ))
   }
   return result
+}).map(name => {
+  if (name.endsWith('Icon.svg')) {
+    let name2 = name.slice(0, -8) + '.svg'
+    fs.renameSync(`${dir}/${name}`, `${dir}/${name2}`)
+    return name2
+  }
+  return name
 }).map(name =>
-`.pi${name.slice(0, -8).replace(/([A-Z])/g, "-$1").toLowerCase()}{
--webkit-mask-image: url(${name});
-mask-image: url(${name});
-}`)
+`.pi-${name.slice(0, -4)}{--i: url(${name})}`)
 fs.writeFileSync(dir + '/style.css', `/*!
 * @shopify/polaris-icons 9.0.0 (https://polaris-icons.shopify.com)
 * Copyright (c) 2017-present Shopify Inc.
 * Licensed under MIT (https://unpkg.com/browse/@shopify/polaris-icons@9.0.0/LICENSE.md)
 */
 .pi{
+-webkit-mask-image: var(--i);
+mask-image: var(--i);
 -webkit-mask-position: center;
 mask-position: center;
 -webkit-mask-repeat: no-repeat;
